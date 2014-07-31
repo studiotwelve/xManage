c11bec0def2060657e6426205e7e643416018cd2
studiotwelve <studio12designs@me.com>
2014-07-30 22:35:27 -0400

    initial
    very undone do not use

--- xManage/AppDelegate.m
+++ xManage/AppDelegate.m
@@ -10,6 +10,176 @@
 
 @implementation AppDelegate
 
+-(void) runScript:(NSString*)scriptName
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/bin/sh"];
+    
+    NSArray *arguments;
+    NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] privateFrameworksPath], scriptName];
+    NSLog(@"shell script path: %@",newpath);
+    arguments = [NSArray arrayWithObjects:newpath, nil];
+    [task setArguments: arguments];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    [task setStandardInput:[NSPipe pipe]];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
+    [task launch];
+    
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *string;
+    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    NSLog (@"script returned:\n%@", string);
+}
+
+-(void) runCommand:(NSString *)commandToRun
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/bin/sh"];
+    
+    NSArray *arguments = [NSArray arrayWithObjects:
+                          @"-c" ,
+                          [NSString stringWithFormat:@"%@", commandToRun],
+                          nil];
+    NSLog(@"run command: %@",commandToRun);
+    [task setArguments: arguments];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    [task setStandardInput:[NSPipe pipe]];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
+    [task launch];
+    
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *string;
+    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    NSLog (@"script returned:\n%@", string);
+}
+
+- (BOOL) runProcessAsAdministrator:(NSString*)scriptPath
+                     withArguments:(NSArray *)arguments
+                            output:(NSString **)output
+                  errorDescription:(NSString **)errorDescription {
+    
+    NSString * allArgs = [arguments componentsJoinedByString:@" "];
+    NSString * fullScript = [NSString stringWithFormat:@"'%@' %@", scriptPath, allArgs];
+    
+    NSDictionary *errorInfo = [NSDictionary new];
+    NSString *script =  [NSString stringWithFormat:@"do shell script \"%@\" with administrator privileges", fullScript];
+    
+    NSAppleScript *appleScript = [[NSAppleScript new] initWithSource:script];
+    NSAppleEventDescriptor * eventResult = [appleScript executeAndReturnError:&errorInfo];
+    
+    // Check errorInfo
+    if (! eventResult)
+    {
+        // Describe common errors
+        *errorDescription = nil;
+        if ([errorInfo valueForKey:NSAppleScriptErrorNumber])
+        {
+            NSNumber * errorNumber = (NSNumber *)[errorInfo valueForKey:NSAppleScriptErrorNumber];
+            if ([errorNumber intValue] == -128)
+                *errorDescription = @"The administrator password is required to do this.";
+        }
+        
+        // Set error message from provided message
+        if (*errorDescription == nil)
+        {
+            if ([errorInfo valueForKey:NSAppleScriptErrorMessage])
+                *errorDescription =  (NSString *)[errorInfo valueForKey:NSAppleScriptErrorMessage];
+        }
+        
+        return NO;
+    }
+    else
+    {
+        // Set output to the AppleScript's output
+        *output = [eventResult stringValue];
+        
+        return YES;
+    }
+}
+
+-(IBAction)startApache:(id)sender
+{
+    NSString * output = nil;
+    NSString * processErrorDescription = nil;
+    BOOL success = [self runProcessAsAdministrator:@"/usr/sbin/apachectl"
+                                     withArguments:[NSArray arrayWithObjects:@"start", nil]
+                                            output:&output
+                                  errorDescription:&processErrorDescription];
+    
+    
+    if (!success) // Process failed to run
+    {
+        // ...look at errorDescription
+        NSLog(@"err: %@", processErrorDescription);
+    }
+    else
+    {
+        // ...process output
+        NSLog(@"output: %@", output);
+    }
+}
+
+-(IBAction)stopApache:(id)sender
+{
+    NSString * output = nil;
+    NSString * processErrorDescription = nil;
+    BOOL success = [self runProcessAsAdministrator:@"/usr/sbin/apachectl"
+                                     withArguments:[NSArray arrayWithObjects:@"stop", nil]
+                                            output:&output
+                                  errorDescription:&processErrorDescription];
+    
+    
+    if (!success) // Process failed to run
+    {
+        // ...look at errorDescription
+        NSLog(@"err: %@", processErrorDescription);
+    }
+    else
+    {
+        // ...process output
+        NSLog(@"output: %@", output);
+    }
+}
+
+-(IBAction)restartApache:(id)sender
+{
+    NSString * output = nil;
+    NSString * processErrorDescription = nil;
+    BOOL success = [self runProcessAsAdministrator:@"/usr/sbin/apachectl"
+                                     withArguments:[NSArray arrayWithObjects:@"restart", nil]
+                                            output:&output
+                                  errorDescription:&processErrorDescription];
+    
+    if (!success) // Process failed to run
+    {
+        // ...look at errorDescription
+        NSLog(@"err: %@", processErrorDescription);
+    }
+    else
+    {
+        // ...process output
+        NSLog(@"output: %@", output);
+    }
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
